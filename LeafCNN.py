274e7f553ad6e349fb48f91a11f6b48ecbf31df7
R2Z1-03 <b2027jytolentino@pshs.edu.ph>
2025-12-21 15:11:29 +0800

    Test code for setting up conda virtual environments as Python interpreters in VSCode: R2Z1-03LEAF (Python 3.10.13) works while R2Z1-03FLOWER (Python 3.10.19) errors. This may be due to incompatible package versions.
    So both model files may just work under R2Z1-03LEAF. R2Z1-03FLOWER will only be fixed and edited once deemed necessary (eg. if there are vastly different versions needed for the flower model).

--- LeafCNN.py
+++ LeafCNN.py
@@ -1,1 +1,17 @@
-# testing
+# This will display "Hello, World!" in the terminal
+print("Hello, World!")
+
+# This will display your system version in the terminal
+import sys
+print(sys.version)
+
+import tensorflow as tf
+from tensorflow import keras
+
+print("tf version", tf.__version__)
+print("keras version", keras.__version__)
+
+print("TensorFlow version: ", tf.__version__)
+print("GPU built with TensorFlow: ", tf.test.is_built_with_cuda())
+print("Can access GPU: ", tf.config.experimental.list_physical_devices('GPU'))
+print('67 poggers nyancat')
