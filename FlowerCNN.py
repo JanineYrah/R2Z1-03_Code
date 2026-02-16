97025a3570ee883b3cc85081052b522b6b15872d
r2z103 <janine.yrah@gmail.com>
2026-02-16 16:16:47 +0800

    February 16, 2026 Afternoon
    Coding of ResNet 50 Flower CNN; Flower CNN Train-Val Iteration 1 (TASK G, H) (Yrah)
    
    - Flower model iteration 1 (mimics leaf model but with 22-node output layer)
    - Trained and validated flower model iteration 1, saved
    - Graph and results gained
    
    Next Plan/s: Replicate train-val process here in the leaf modekl; conduct analysis and hyperparameter tuning on flower model.

--- FlowerCNN.py
+++ FlowerCNN.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
 from tensorflow import keras
-# import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
+import keras
+import os
+from PIL import Image
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 
 print("SYSTEM/PACKAGE INFORMATION:")
 print("TensorFlow version: ", tf.__version__)
@@ -8,6 +14,83 @@ print("keras version", keras.__version__)
 print("GPU built with TensorFlow: ", tf.test.is_built_with_cuda())
 print("Can access GPU: ", tf.config.experimental.list_physical_devices('GPU'))
 print("TROUBLESHOOT DONE")
+
+'''TRAIN-VALIDATE-TEST SPLIT'''
+plant_categories_F = ["AAU", "ACO", "AMA", "BPU", "CFI", "CJA", "DRE", "IBI", "LLE", "LPU", "MDI",
+"MPU", "MQU", "PDU", "PIN", "PSA", "PVU", "SAL", "SOB", "SOC", "SSA", "TIN"]
+flower_directory = '/home/r2z103/res_dataset/Flower' # all png and resized to 224x224
+
+print("DATASETS:")
+print("FLOWER")
+flower_train_ds, flower_val_test_ds = tf.keras.utils.image_dataset_from_directory(flower_directory, labels = "inferred", label_mode = "int", class_names = plant_categories_F,
+                                                                                  validation_split = 0.3, subset = "both", image_size = (224, 224), color_mode = "rgb", shuffle = True, seed = 1, batch_size = None)
+flower_val_batches = tf.data.experimental.cardinality(flower_val_test_ds)
+flower_test_ds = flower_val_test_ds.take(flower_val_batches // 2)
+flower_val_ds = flower_val_test_ds.skip(flower_val_batches // 2)
+
+# Image Augmentation (for necessary classes)
+augmentation_pipeline = [
+    tf.keras.layers.RandomRotation([-0.20, 0.20]),
+    tf.keras.layers.RandomFlip(), # horizontal_and_vertical by default
+    tf.keras.layers.RandomBrightness((0.8, 1.2))
+]
+
+def conditional_augmentation(image, label, target):
+
+    def augment():
+        augmented = image
+        for layer in augmentation_pipeline:
+            augmented = layer(augmented, training=True)
+        return augmented, label
+
+    def no_augment():
+        return image, label
+
+    return tf.cond(
+        tf.equal(label, target),
+        augment,
+        no_augment
+    )
+
+def count_dataset(ds):
+    return sum(1 for _ in ds)
+
+flower_class2_ds = flower_train_ds.filter(lambda img, lbl: tf.equal(lbl, 2))
+flower_class14_ds = flower_train_ds.filter(lambda img, lbl: tf.equal(lbl, 14))
+
+flower_class2_aug_ds = flower_class2_ds.map(
+    lambda img, lbl: conditional_augmentation(img, lbl, 2),
+    num_parallel_calls=tf.data.AUTOTUNE
+)
+flower_train_ds = flower_train_ds.concatenate(flower_class2_aug_ds)
+flower_train_ds = flower_train_ds.shuffle(100)
+
+flower_class14_aug_ds = flower_class14_ds.map(
+    lambda img, lbl: conditional_augmentation(img, lbl, 14),
+    num_parallel_calls=tf.data.AUTOTUNE
+)
+flower_train_ds = flower_train_ds.concatenate(flower_class14_aug_ds)
+flower_train_ds = flower_train_ds.shuffle(100)
+
+dataset_names = [flower_train_ds, flower_val_ds, flower_test_ds]
+
+for name in dataset_names:
+    labels_list = []
+    for images, labels in name:
+        labels_list.append(labels.numpy())
+    all_labels = np.array(labels_list)
+    unique_labels, counts = np.unique(all_labels, return_counts=True)
+    print(f"\n{name} class distribution:")
+    for label, count in zip(unique_labels, counts):
+        print(f"Class {label}: {count} images")
+
+flower_train_ds = flower_train_ds.batch(32).prefetch(buffer_size = tf.data.AUTOTUNE)
+flower_val_ds = flower_val_ds.batch(32).prefetch(buffer_size = tf.data.AUTOTUNE)
+flower_test_ds = flower_test_ds.batch(32).prefetch(buffer_size = tf.data.AUTOTUNE)
+
+for images, labels in flower_train_ds.take(1):
+    print("Images:", images.shape)
+    print("Labels:", labels.shape)
 
 '''MODEL PROPER'''
 IMG_SIZE = (224, 224, 3) # Set image size
@@ -20,7 +103,6 @@ base_model.trainable = False
 # Preprocessing and main model
 model = tf.keras.Sequential([
     base_model, # include_top may be False in the base_model if the immediately preceeding layer before it differs in input_shape. keep true if they are the same
-    # Additional layers
     tf.keras.layers.GlobalAveragePooling2D(),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.0005)),
@@ -30,24 +112,28 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.0005)),
     tf.keras.layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.0005)),
     tf.keras.layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.0005)),
-    tf.keras.layers.Dense(25) # output layer; modified for 25 layers
+    tf.keras.layers.Dense(22) # output layer; modified for 22 layers
     ])
 
 # Compile model
+for layer in model.layers[1:]: # skip rescale and base resnet50 model
+    layer.trainable = True
+
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
 loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
 
 model.summary()
-model.save("leaf_CNN-1.keras")
 
-'''
 # TRAIN-VAL
 number_epochs = 30
-train_val_history = model.fit(img_train, label_train,
+train_val_history = model.fit(flower_train_ds,
                               epochs = number_epochs,
-                              batch_size = 32,
-                              validation_data = (x_val, y_val)
+                              validation_data = (flower_val_ds)
                               )
+
+model.save("flower_CNN-1.keras")
+tf.keras.utils.plot_model(model, to_file = "flower_1.png", show_shapes = True, show_dtype = False, show_layer_names = False, show_layer_activations = False, show_trainable = False)
+tf.keras.utils.plot_model(model, to_file = "flower_1_simple.png", show_shapes = True, show_dtype = True, show_layer_names = True, show_layer_activations = True, show_trainable = True)
 
 # Evaluate train-val results
 acc = train_val_history.history['accuracy']
@@ -71,10 +157,11 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 
+'''
 # BAYESIAN OPTIMIZATION
 
 # Load saved CNN
-tuned_model = tf.keras.models.load_model("leaf_CNN-1.keras")
+tuned_model = tf.keras.models.load_model("flower_CNN-1.keras")
 
 # Build hypermodel
 def build_model(hp):
@@ -96,10 +183,8 @@ def build_model(hp):
     
     return model
 
-tuner = keras.tuner.BayesianOptimization(build_model, objective='val_accuracy',
-                     max_epochs=10)
-tuner.search(img_train, label_train, epochs=30,
-validation_split=0.2)
+tuner = keras.tuner.BayesianOptimization(build_model, objective='val_accuracy', max_epochs=10)
+tuner.search(flower_train_ds, epochs=30, validation_data=flower_val_ds)
 
 # Get and print optimal hyperparameters
 best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
